9881c2dc194cd94429a91f41bb66620cca0bec13
pappelt <mail@services.bad-science.net>
2011-06-23 15:11:09 +0200

    added config.php, thus modified merge.php 	new file:   config.php 	modified:   merge.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	//$config['downloadFolder'] = "http://pdfmerger.bad-science.net/downloads/";
+	$config['downloadFolder'] = "http://localhost/~pa/pdfMerger/downloads/";
+?>
--- merge.php
+++ merge.php
@@ -1,38 +1,41 @@
 <?php
-require_once('fpdf17/fpdf.php');
-require_once('fpdi-1.4.1/fpdi.php');
+	require_once('fpdf/src/fpdf.php');
+	require_once('fpdi/fpdi.php');
+	require_once('config.php');
 
-class concat_pdf extends fpdi {
-	var $files = array();
+	class concat_pdf extends fpdi {
+		var $files = array();
 
-	function concat_pdf($orientation='P',$unit='mm',$format='A4') {
-		parent::fpdi($orientation,$unit,$format);
-	}
+		function concat_pdf($orientation='P',$unit='mm',$format='A4') {
+			parent::fpdi($orientation,$unit,$format);
+		}
 
-	function setFiles($files) {
-		$this->files = $files;     
-	}
+		function setFiles($files) {
+			$this->files = $files;     
+		}
 
-	function concat() {
-		foreach($this->files AS $file) {
-			$pagecount = $this->setSourceFile($file);
-			for ($i = 1; $i <= $pagecount; $i++) {
-				$tplidx = $this->ImportPage($i);$this->AddPage();
-				$this->useTemplate($tplidx);
+		function concat() {
+			foreach($this->files AS $file) {
+				$pagecount = $this->setSourceFile($file);
+				for ($i = 1; $i <= $pagecount; $i++) {
+					$tplidx = $this->ImportPage($i);$this->AddPage();
+					$this->useTemplate($tplidx);
+				}
 			}
 		}
 	}
-}
 
-$files = array();
-foreach ($_GET as $file) {
-	array_push(&$files, "uploads/{$file}");
-}
+	$files = array();
+	foreach ($_GET as $file) {
+		array_push(&$files, "uploads/{$file}");
+	}
 
-$pdf= new concat_pdf();
-$pdf->setFiles($files);
-$pdf->concat();
-$filename = rand();
-$pdf->Output("downloads/{$filename}.pdf","F");
-echo "Ready, Download <a href=\"http://pdfmerger.bad-science.net/downloads/{$filename}.pdf\">this</a>";
+	$pdf= new concat_pdf();
+	$pdf->setFiles($files);
+	$pdf->concat();
+	$filename = rand();
+	$pdf->Output("downloads/{$filename}.pdf","F");
+
+	var_dump($config['downloadFolder']);
+	echo "Ready, Download <a href=\"{$config['downloadFolder']}{$filename}.pdf\">this</a>";
 ?>
